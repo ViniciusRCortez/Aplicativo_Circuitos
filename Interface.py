376d728ac53721c574051adfee2f9ac14b213582
Vinícius Cortez <vinisicius@gmail.com>
2021-11-21 19:40:14 -0300

    final_edition

--- Interface.py
+++ Interface.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
-from Resposta import grafico
+from Resposta import grafico_tensão, grafico_corrente
+
 
 # Layouts:
 def Inicial():
@@ -9,6 +10,8 @@ def Inicial():
         [sg.Checkbox('Circuito RC', key='RC', size=(30, 0))],
         [sg.Checkbox('Circuito RLC série', key='RLCs', size=(30, 0))],
         [sg.Checkbox('Circuito RLC paralelo', key='RLCp', size=(30, 0))],
+        [sg.Text('Calcular:'), sg.Checkbox('Tensão', key='Tensão', size=(10, 0)),
+         sg.Checkbox('Corrente', key='Corrente', size=(10, 0))],
         [sg.Button('Próximo', size=(10, 0))]
     ]
     return sg.Window('Circuitos 1 - UFC', layout=layout, finalize=True)
@@ -20,7 +23,7 @@ def RC():
         [sg.Text('Valor da Tensão Final da Fonte em Volts:', size=(30, 0)),
          sg.Input(key='Vf', size=(10, 0))],
         [sg.Text('Tempo da simulação em MiliSegundos:', size=(30, 0)), sg.Input(key='tempo', size=(10, 0))],
-        [sg.Text('Valor da Tensão de Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Tensão Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
         [sg.Text('Valor do Resistor em Ohms', size=(30, 0)), sg.Input(key='R', size=(10, 0))],
         [sg.Text('Valor do Capacitor em MicroFaradays:', size=(30, 0)), sg.Input(key='C', size=(10, 0))],
         [sg.Button('Calcular', size=(10, 0)), sg.Button('Voltar', size=(10, 0))]
@@ -34,7 +37,7 @@ def RL():
         [sg.Text('Valor da Tensão Final da Fonte em Volts:', size=(30, 0)),
          sg.Input(key='Vf', size=(10, 0))],
         [sg.Text('Tempo da simulação em MiliSegundos:', size=(30, 0)), sg.Input(key='tempo', size=(10, 0))],
-        [sg.Text('Valor da Tensão de Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Tensão Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
         [sg.Text('Valor do Resistor em Ohms', size=(30, 0)), sg.Input(key='R', size=(10, 0))],
         [sg.Text('Valor do Indutor em MileHenrys:', size=(30, 0)), sg.Input(key='L', size=(10, 0))],
         [sg.Button('Calcular', size=(10, 0)), sg.Button('Voltar', size=(10, 0))]
@@ -48,10 +51,13 @@ def RLCs():
         [sg.Text('Valor da Tensão Final da Fonte em Volts:', size=(30, 0)),
          sg.Input(key='Vf', size=(10, 0))],
         [sg.Text('Tempo da simulação em MiliSegundos:', size=(30, 0)), sg.Input(key='tempo', size=(10, 0))],
-        [sg.Text('Valor da Tensão de Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Tensão Inicial do Capacitor em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Corrente Inicial do Indutor em Amperes:', size=(30, 0)), sg.Input(key='Ii', size=(10, 0))],
         [sg.Text('Valor do Resistor em Ohms', size=(30, 0)), sg.Input(key='R', size=(10, 0))],
         [sg.Text('Valor do Capacitor em MicroFaradays:', size=(30, 0)), sg.Input(key='C', size=(10, 0))],
         [sg.Text('Valor do Indutor em MileHenrys:', size=(30, 0)), sg.Input(key='L', size=(10, 0))],
+        [sg.Text('Tensão sobre o:'), sg.Checkbox('Capacitor', key='Cap', size=(10, 0)),
+         sg.Checkbox('Indutor', key='Ind', size=(10, 0))],
         [sg.Button('Calcular', size=(10, 0)), sg.Button('Voltar', size=(10, 0))]
     ]
     return sg.Window('Circuitos 1 - RLC série', layout=layout, finalize=True)
@@ -60,13 +66,16 @@ def RLCs():
 def RLCp():
     sg.change_look_and_feel('DarkBlue3')
     layout = [
-        [sg.Text('Valor da Tensão Final da Fonte em Volts:', size=(30, 0)),
+        [sg.Text('Valor da Corrente Final da Fonte em Amperes:', size=(30, 0)),
          sg.Input(key='Vf', size=(10, 0))],
         [sg.Text('Tempo da simulação em MiliSegundos:', size=(30, 0)), sg.Input(key='tempo', size=(10, 0))],
-        [sg.Text('Valor da Tensão de Inicial em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Tensão Inicial do Capacitor em Volts:', size=(30, 0)), sg.Input(key='Vi', size=(10, 0))],
+        [sg.Text('Valor da Corrente Inicial do Indutor em Amperes:', size=(30, 0)), sg.Input(key='Ii', size=(10, 0))],
         [sg.Text('Valor do Resistor em Ohms', size=(30, 0)), sg.Input(key='R', size=(10, 0))],
         [sg.Text('Valor do Capacitor em MicroFaradays:', size=(30, 0)), sg.Input(key='C', size=(10, 0))],
         [sg.Text('Valor do Indutor em MileHenrys:', size=(30, 0)), sg.Input(key='L', size=(10, 0))],
+        [sg.Text('Tensão sobre o:'), sg.Checkbox('Capacitor', key='Cap', size=(10, 0)),
+         sg.Checkbox('Indutor', key='Ind', size=(10, 0))],
         [sg.Button('Calcular', size=(10, 0)), sg.Button('Voltar', size=(10, 0))]
     ]
     return sg.Window('Circuitos 1 - RLC paralelo', layout=layout, finalize=True)
@@ -83,7 +92,20 @@ while True:
         break
     # Proxima Janela:
     if event == 'Próximo':
-        #Circuito RC
+        if value['Corrente'] == False and value['Tensão'] == False:
+            janela1.hide()
+            janela1 = Inicial()
+            value['RC'] = value['RL'] = value['RLCs'] = value['RLCp'] = False
+            tipo = 'Inicio'
+        elif value['Corrente'] == True and value['Tensão'] == True:
+            janela1.hide()
+            janela1 = Inicial()
+            value['RC'] = value['RL'] = value['RLCs'] = value['RLCp'] = False
+            tipo = 'Inicio'
+        elif value['Tensão']:
+            escolha = 'Tensão'
+        else:
+            escolha = 'Corrente'
         if value['RC'] == True and value['RL'] == value['RLCs'] == value['RLCp'] == False:
             janela2 = RC()
             janela1.hide()
@@ -101,14 +123,15 @@ while True:
             janela1.hide()
             tipo = 'RLCp'
         else:
-            sg.popup('Escolha somente uma opção')
-    #Voltando para janela anterior:
+            sg.popup('Escolha somente um opção valida')
+    # Voltando para janela anterior:
     if event == 'Voltar':
         janela2.hide()
         janela1 = Inicial()
         tipo = 'Inicio'
-    #Plotando grafico:
+    # Plotando grafico:
     if event == 'Calcular':
+        # Certificando qual sobre qual carga busca a resposta:
         if tipo == 'RC':
             # Extraindo dados:
             lista = ('R', 'C', 'Vf', 'tempo', 'Vi')
@@ -123,8 +146,12 @@ while True:
             value['Vf'] = float(value['Vf'])
             value['tempo'] = (float(value['tempo'])) / (10 ** 3)
             value['Vi'] = float(value['Vi'])
-            grafico(value['R'], value['C'], 0, value['Vf'], value['Vi'],
-                value['tempo'], tipo)
+            if escolha == 'Tensão':
+                grafico_tensão(value['R'], value['C'], 0, value['Vf'], value['Vi'],
+                               value['tempo'], tipo, 0)
+            elif escolha == 'Corrente':
+                grafico_corrente(value['R'], value['C'], 0, value['Vf'], value['Vi'],
+                                 value['tempo'], tipo, 0)
         elif tipo == 'RL':
             lista = ('R', 'L', 'Vf', 'tempo', 'Vi')
             for i in lista:
@@ -134,13 +161,23 @@ while True:
             if value['tempo'] == 0:
                 value['tempo'] = 1  # Caso tempo esteja em branco sera 1ms
             value['R'] = float(value['R'])
-            value['L'] = (float(value['L']))/(10**3)
+            value['L'] = (float(value['L'])) / (10 ** 3)
             value['Vf'] = float(value['Vf'])
             value['tempo'] = (float(value['tempo'])) / (10 ** 3)
             value['Vi'] = float(value['Vi'])
-            grafico(value['R'], 0, value['L'], value['Vf'], value['Vi'],
-                    value['tempo'], tipo)
+            if escolha == 'Tensão':
+                grafico_tensão(value['R'], 0, value['L'], value['Vf'], value['Vi'],
+                               value['tempo'], tipo, 0)
+            elif escolha == 'Corrente':
+                grafico_corrente(value['R'], 0, value['L'], value['Vf'], value['Vi'],
+                                 value['tempo'], tipo, 0)
+
         elif tipo == 'RLCs':
+            if [value['Cap'], value['Ind']] == [True, True] or [value['Cap'], value['Ind']] == [False, False]:
+                sg.popup('Escolha somente entre Indutor ou Capacitor.')
+                janela2.hide()
+                janela1 = Inicial()
+                tipo = 'Inicio'
             lista = ('R', 'C', 'L', 'Vf', 'tempo', 'Vi')
             for i in lista:
                 if value[i] == None or value[i] == '':  # Zera os valores em branco
@@ -154,9 +191,19 @@ while True:
             value['Vf'] = float(value['Vf'])
             value['tempo'] = (float(value['tempo'])) / (10 ** 3)
             value['Vi'] = float(value['Vi'])
-            grafico(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
-                    value['tempo'], tipo)
+            if tipo == 'RLCs':
+                if escolha == 'Tensão':
+                    grafico_tensão(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
+                                   value['tempo'], tipo, [value['Cap'], value['Ind']], value['Ii'])
+                elif escolha == 'Corrente':
+                    grafico_corrente(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
+                                     value['tempo'], tipo, [value['Cap'], value['Ind']], value['Ii'])
         elif tipo == 'RLCp':
+            if [value['Cap'], value['Ind']] == [True, True] or [value['Cap'], value['Ind']] == [False, False]:
+                sg.popup('Escolha somente entre Indutor ou Capacitor.')
+                janela2.hide()
+                janela1 = Inicial()
+                tipo = 'Inicio'
             lista = ('R', 'C', 'L', 'Vf', 'tempo', 'Vi')
             for i in lista:
                 if value[i] == None or value[i] == '':  # Zera os valores em branco
@@ -170,10 +217,14 @@ while True:
             value['Vf'] = float(value['Vf'])
             value['tempo'] = (float(value['tempo'])) / (10 ** 3)
             value['Vi'] = float(value['Vi'])
-            grafico(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
-                    value['tempo'], tipo)
+            if tipo == 'RLCp':
+                if escolha == 'Tensão':
+                    grafico_tensão(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
+                                   value['tempo'], tipo, [value['Cap'], value['Ind']], value['Ii'])
+                elif escolha == 'Corrente':
+                    grafico_corrente(value['R'], value['C'], value['L'], value['Vf'], value['Vi'],
+                                     value['tempo'], tipo, [value['Cap'], value['Ind']], value['Ii'])
         else:
             sg.popup('Valores invalidos, tente novamente.')
             janela2.hide()
             janela1 = Inicial()
-
--- Resposta.py
+++ Resposta.py
@@ -4,9 +4,22 @@ Responder a EDO para a resposta tensão dos casos RC, RL e RLC
 from math import exp
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 
-def grafico(R, C, L, Vf, Vi, tempo, tipo):
+def RLCs(y, t, R, L, C, V):
+    vc, x = y  # x = dvc/dt
+    dxdt = [x, (1 / (L * C)) * V - (R / L) * x - (1 / (L * C)) * vc]
+    return dxdt
+
+
+def RLCp(y, t, R, L, C, V):
+    vc, x = y  # x = dvc/dt
+    dxdt = [x, V*(1/(L*C)) - (1 / (R * C)) * x - (1 / (L * C)) * vc]
+    return dxdt
+
+
+def grafico_tensão(R, C, L, Vf, Vi, tempo, tipo, Sobre=[0, 0], Ii=0):
     """
     Plota um grafico interatico depois de passado os valores das variaveis.
     :param R: Valor do Resistor.
@@ -15,43 +28,137 @@ def grafico(R, C, L, Vf, Vi, tempo, tipo):
     :param Vf: Valor da Tensão na fonte, durante o regime não estacionario(t>0+).
     :param Vi: Condição inicial da fonte.
     :param tipo: Tipo do circuito.
-    :param tempo: Valor do Resistor.
+    :param tempo: Tempo de simulação.
+    :param sobre: Se é a saída sobre o capacitor ou o indutor.
+    :param Ii: Corrente inicial do indutor.
     """
-    n = tempo / 100
-
-    #   RC ou RL Serie
-    if tipo == 'RC':
-        Resultado = lambda t: Vf + ((Vi - Vf) * (exp(-(1 / (R * C)) * t))) # Vc = Vf + (Vi - Vf)e^(-1/rc)t
-    elif tipo == 'RL':
-        If = Vf/R
-        Ii = Vi/R
-        Corrente = lambda t: If + ((Ii - If) * (exp((-(R/L)) * t)))  # Il = If + (Ii - If)e^(-R/L)t
-        Resultado = lambda t: Vf-R*Corrente(t)                      #Vl = Vf - Vr = Vf - R*I
+    n = tempo / 1000
     x = np.arange(0, tempo + n, n)
-    V = np.array([Resultado(t) for t in x])
 
-    plt.ion()
-    plt.cla()
-    plt.clf()
-    plt.tight_layout()
-    plt.style.use('ggplot')
-    plt.plot(x * 1000, V, 'r-', label='Vcarga')
-    plt.title('Tesão sobre a carga - Serie')
-    plt.xlabel('t(ms)')
-    plt.ylabel('Vcarga(V)')
-    plt.legend(loc='best')
-    plt.pause(0.1)
-    plt.ioff()
-    plt.show()
+    if tipo == 'RC':
+        Resultado = lambda t: Vf + ((Vi - Vf) * (exp(-(1 / (R * C)) * t)))  # Vc = Vf + (Vi - Vf)e^(-1/rc)t
+    elif tipo == 'RL':
+        If = Vf / R
+        Ii = Vi / R
+        Corrente = lambda t: If + ((Ii - If) * (exp((-(R / L)) * t)))  # Il = If + (Ii - If)e^(-R/L)t
+        Resultado = lambda t: Vf - R * Corrente(t)  # Vl = Vf - Vr = Vf - R*I
+    # RLC serie
+    elif tipo == 'RLCs' and Sobre == [True, False]:  # Tensão sobre o Capacitor
+        y0 = [Vi, Ii]
+        sol = odeint(RLCs, y0, x, args=(R, L, C, Vf))
+        V = sol[:, 0]
+    elif tipo == 'RLCs' and Sobre == [False, True]:  # Tensão sobre o Indutor
+        y0 = [Vi, Ii]
+        sol = odeint(RLCs, y0, x, args=(R, L, C, Vf))
+        Vc = sol[:, 0]
+        Il = sol[:, 1] * C
+        Vf_novo = np.ones_like(Vc) * Vf
+        V = Vf_novo - R * Il - Vc
+    elif tipo == 'RLCp':  # Tensão sobre o Capacitor, para paralelo é a mesma
+        y0 = [Vi, Ii]
+        sol = odeint(RLCp, y0, x, args=(R, L, C, Vf))
+        Il_linha = sol[:, 1]
+        V = L*Il_linha
+    if tipo == 'RC' or tipo == 'RL':
+        V = np.array([Resultado(t) for t in x])
+
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Vcarga')
+        plt.title('Tesão sobre a carga')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Vcarga(V)')
+        plt.legend(loc='best')
+        plt.show()
+    elif tipo == 'RLCs':
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Vcarga')
+        plt.title('Tesão sobre a carga - Serie')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Vcarga(V)')
+        plt.legend(loc='best')
+        plt.show()
+    elif tipo == 'RLCp':
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Vcarga')
+        plt.title('Tesão sobre a carga - Paralelo')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Vcarga(V)')
+        plt.legend(loc='best')
+        plt.show()
 
 
-"""R = 10
-C = 1
-C /= 1000000
-L = 0
-L /= 1000
-Vi = 10
-V0 = 0
-tempo = 0.1
-tempo /= 1000
-grafico(R, C, L, Vi, V0, tempo)"""
+def grafico_corrente(R, C, L, Vf, Vi, tempo, tipo, Sobre=[0, 0], Ii=0):
+    """
+       Plota um grafico interatico depois de passado os valores das variaveis.
+       :param R: Valor do Resistor.
+       :param C: Valor do Capacitor.
+       :param L: Valor do Indutor.
+       :param Vf: Valor da Tensão na fonte, durante o regime não estacionario(t>0+).
+       :param Vi: Condição inicial da fonte.
+       :param tipo: Tipo do circuito.
+       :param tempo: Tempo de simulação.
+       :param sobre: Se é a saída sobre o capacitor ou o indutor.
+       :param Ii: Corrente inicial do indutor.
+       """
+    n = tempo / 1000
+    x = np.arange(0, tempo + n, n)
+
+    if tipo == 'RC':
+        Vc = lambda t: Vf + ((Vi - Vf) * (exp(-(1 / (R * C)) * t)))  # Vc = Vf + (Vi - Vf)e^(-1/rc)t
+        Resultado = lambda t: (Vf - Vc(t))/R
+    elif tipo == 'RL':
+        If = Vf / R
+        Ii = Vi / R
+        Resultado = lambda t: If + ((Ii - If) * (exp((-(R / L)) * t)))  # Il = If + (Ii - If)e^(-R/L)t
+    # RLC serie
+    elif tipo == 'RLCs':  # Tensão sobre o Capacitor
+        y0 = [Vi, Ii]
+        sol = odeint(RLCs, y0, x, args=(R, L, C, Vf))
+        Vc_linha = sol[:, 1]
+        V = C*Vc_linha
+    elif tipo == 'RLCp' and Sobre == [False, True]:  # Corrente sobre o Indutor
+        y0 = [Vi, Ii]
+        sol = odeint(RLCp, y0, x, args=(R, L, C, Vf))
+        V = sol[:, 0]
+    elif tipo == 'RLCp' and Sobre == [True, False]:  # Corrente sobre o Capacitor
+        y0 = [Vi, Ii]
+        sol = odeint(RLCp, y0, x, args=(R, L, C, Vf))
+        Il = sol[:, 0]
+        Il_linha = sol[:, 1]
+        Vc = L*Il_linha
+        V = Vf - Vc/R - Il
+    if tipo == 'RC' or tipo == 'RL':
+        V = np.array([Resultado(t) for t in x])
+
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Icarga')
+        plt.title('Corrente sobre a carga')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Icarga(A)')
+        plt.legend(loc='best')
+        plt.show()
+    elif tipo == 'RLCs':
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Icarga')
+        plt.title('Corrente sobre a carga - Serie')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Icarga(A)')
+        plt.legend(loc='best')
+        plt.show()
+    elif tipo == 'RLCp':
+        plt.tight_layout()
+        plt.style.use('ggplot')
+        plt.plot(x * 1000, V, 'r-', label='Icarga')
+        plt.title('Corrente sobre a carga - Paralelo')
+        plt.xlabel('t(ms)')
+        plt.ylabel('Icarga(A)')
+        plt.legend(loc='best')
+        plt.show()
+
+
+
